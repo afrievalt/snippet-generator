88780c48909a9fd0ef2087ebb6987e712432d211
Andy Frievalt <afrievalt@yahoo.com>
2024-08-16 19:45:20 -0500

    fix chose variable

--- src/components/Placeholders/ChooseVariable.tsx
+++ src/components/Placeholders/ChooseVariable.tsx
@@ -4,33 +4,30 @@ import { variables } from "./variables";
 
 function ChooseVariable() {
   const context = useContext(Context);
-  const handleChange =  (e: ChangeEvent<HTMLInputElement>) => {
+  const handleChange = (e: ChangeEvent<HTMLInputElement>) => {
     context.setVariable(e.target.value);
   };
-  
+
   return (
-    <fieldset >
+    <fieldset>
       {variables.map((row) => (
-        <div key={row.variable} className="item">
+        <div key={row.variable} className="item inline" >
+          <input
+            checked={context.variable === row.variable}
+            onChange={handleChange}
+            type="radio"
+            id={row.variable}
+            name="variable"
+            value={row.variable}
+          />
           <label htmlFor={row.variable}>
-            <input
-              checked={context.variable === row.variable}
-              onChange={handleChange}
-              type="radio"
-              id={row.variable}
-              name="variable"
-              value={row.variable}
-            />
-            &nbsp;
             {row.variable}
           </label>
           <div className="variables__description">{row.description}</div>
         </div>
-      ))}      
+      ))}
     </fieldset>
   );
 }
 
 export default ChooseVariable;
-
-
